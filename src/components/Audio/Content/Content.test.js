5d7af98de6139ebb086ee3109d726b6c9656f2da
Raphael P <raphael.prasquier@imagine-app.fr>
2019-11-03 11:18:46 +0100

    Fix setup

--- src/components/Audio/Content/Content.test.js
+++ src/components/Audio/Content/Content.test.js
@@ -7,7 +7,8 @@ describe('Content', () => {
   it('renders correctly', () => {
     const props = {
       title: 'test',
-      body: '<p>test</p>'
+      body: '<p>test</p>',
+      audioUrl: 'https://www.dropbox.com/s/gwxxoei8t7r5blo/2019-05-21%20Ph.%20VAL.mp3?dl=1'
     };
 
     const tree = renderer.create(<Content {...props} />).toJSON();
